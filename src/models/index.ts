f30f782ae0d95c3c0c566aa86b00bb4c1b421853
Juan Barrientos <assacreed100@gmail.com>
2022-05-18 16:30:47 -0300

    index de modelos añadido

---
+++ src/models/index.ts
@@ -0,0 +1,19 @@
+const Categories = require('./categories.ts');
+const PollValues = require('./poll_values.ts');
+const Polls = require('./polls.ts');
+const Posts = require('./posts.ts');
+const Provinces = require('./provinces.ts');
+const SubscriptionPrices = require('./subscriptions_prices.ts');
+const UserVotes = require('./user_votes.ts');
+const User = require('./user.ts');
+
+module.exports = {
+  Categories,
+  PollValues,
+  Polls,
+  Provinces,
+  SubscriptionPrices,
+  UserVotes,
+  User,
+  Posts,
+};
