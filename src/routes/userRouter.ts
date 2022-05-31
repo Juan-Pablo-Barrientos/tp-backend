9b5b26cf055aaacedfddd26278fdfebf4c5e1720
Juan Barrientos <assacreed100@gmail.com>
2022-05-30 23:30:07 -0300

    getAll añadido

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -2,6 +2,6 @@ import express from "express";
 const router = express.Router();
 import * as userControllers from "../controllers/userControllers";
 
-router.get("/", userControllers.getUserById);
+router.get("/", userControllers.getAllUser);
 
 export default router;
