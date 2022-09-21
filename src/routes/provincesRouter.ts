255869a37948e090d80bf4be70cf7e6fe9cfe7bb
@Juan-Pablo-Barrientos <jpbarrientosros@gmail.com>
2022-09-21 19:14:30 -0300

    updated provinces delete and edit

--- src/routes/provincesRouter.ts
+++ src/routes/provincesRouter.ts
@@ -5,5 +5,7 @@ import * as provincesControllers from "../controllers/provincesControllers";
 router.get("/:id", provincesControllers.getProvincesById);
 router.get("/", provincesControllers.getAllProvinces);
 router.post("/", provincesControllers.addProvinces);
+router.put("/:id", provincesControllers.updateProvinces);
+router.delete("/:id", provincesControllers.deleteProvinces);
 
 export default router;
